9eb1447b9b9448d543e4461d28e74cab3576ce4c
Abhiram Krishna <abhiram.krish@gmail.com>
2026-01-20 02:32:16 -0500

    Remove unused Whop boilerplate, simplify to chatbot only

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { WhopApp } from "@whop/react/components";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,7 +27,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<WhopApp>{children}</WhopApp>
+				{children}
 			</body>
 		</html>
 	);
